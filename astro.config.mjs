4df9091445df12c3cbf409caee4b18f87f51955e
Noah Locke <noahlocke@Noahs-Air.lan>
2023-12-20 22:03:27 -0600

    removed lf

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,10 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import alpinejs from "@astrojs/alpinejs";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), alpinejs()]
+  integrations: [tailwind(), alpinejs(), sitemap()],
+  site: 'https://www.seasonscounselingllc.org',
 });
